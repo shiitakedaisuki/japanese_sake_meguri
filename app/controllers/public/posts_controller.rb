23e41e1afdb227587193fa15c9031cf973335efa
shiitakedaisuki <129662461+shiitakedaisuki@users.noreply.github.com>
2023-06-10 14:13:13 +0000

    Addpublicposts1

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,46 @@
 class Public::PostsController < ApplicationController
+  
+  def new
+    @post = Post.new
+  end
+  
+  # 投稿データの保存
+  def create
+    @post = Post.new(image_params)
+    @post.user_id = current_user.id
+    if @post.valid?
+      @post.save
+      flash[:notice] = "投稿が成功しました"
+      redirect_to public_post_path(@post)
+    else
+      render :new
+    end
+  end
+  
+  
+  def index
+    @post = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to public_posts_path
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def image_params
+    params.require(:post).permit(:name, :image, :review)
+  end
+  
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,27 +1,19 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    public_posts_path
+  end
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  protected
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
 end
