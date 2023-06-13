5f0a45a7a506ba7d38b4760f9334183a7806f577
shiitakedaisuki <129662461+shiitakedaisuki@users.noreply.github.com>
2023-06-13 07:07:50 +0000

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_13_031516) do
+ActiveRecord::Schema.define(version: 2023_06_13_065547) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,13 @@ ActiveRecord::Schema.define(version: 2023_06_13_031516) do
     t.integer "user_id"
     t.integer "post_id"
     t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
